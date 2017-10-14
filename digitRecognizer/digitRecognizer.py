db33c027220580372a573f0582521304b2cb17bf
jsonzhang <enjoy.doing.19@gmail.com>
2017-10-14 12:07:20 +0800

    use neural network but didnt perform as well as prediction

--- digitRecognizer/digitRecognizer.py
+++ digitRecognizer/digitRecognizer.py
@@ -2,33 +2,38 @@ import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
+import theano as theano
+from sklearn.neural_network import MLPClassifier
 
 data = pd.read_csv('./data/train.csv', index_col=False)
 test_data = pd.read_csv('./data/test.csv', index_col=False)
 
 y = data['label']
-X = data.loc[:, ~(data == 0).all()].drop('label', axis=1)
-X_target = test_data.loc[:, ~(data == 0).all()]
+X = data.drop('label', axis=1)
+X_target = test_data
+
+# .loc[:, ~(data == 0).all()]
+
 newX = X.applymap(lambda x: 0 if x == 0 else 1)
 X_target = X_target.applymap(lambda x: 0 if x == 0 else 1)
 X_target.index += 1
 
 X_train, X_test, y_train, y_test = train_test_split(newX, y, test_size=0.05, random_state=True)
 
-forrest_params = dict(
-    max_depth = [n for n in range(9, 14)],
-    min_samples_split = [n for n in range(4, 11)],
-    min_samples_leaf = [n for n in range(2, 5)],
-    n_estimators = [n for n in range(10, 60, 10)]
-)
+cols = list(X_train)
+nunique = X_train.apply(pd.Series.nunique)
+cols_to_drop = nunique[nunique == 1].index
+
+X_train = X_train.drop(cols_to_drop, axis=1)
+X_test = X_test.drop(cols_to_drop, axis=1)
+X_target = X_target.drop(cols_to_drop, axis=1)
 
 # logic = GridSearchCV(estimator=RandomForestClassifier(), param_grid=forrest_params, cv=5)
-logic = RandomForestClassifier()
 # logic = LogisticRegression()
+logic = MLPClassifier(activation='tanh')
 logic.fit(X_train, y_train)
 
 result = logic.predict(X_target)
-
 submission = pd.DataFrame({
     "ImageId": X_target.index,
     "Label": result
